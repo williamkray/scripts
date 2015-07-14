29e699790569951232d10c795c33d2454255e590
William Kray <william.kray@gmail.com>
2015-07-14 11:34:44 -0700

    remove updatemx

--- updatemx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current=`curl whatismyip.akamai.com`
-record=`host mx.kray.ca | awk -F ' ' '{print $NF}'
-
-if ![[ "$current" == "$record" ]]
-then
-    wget -O - http://freedns.afraid.org/dynamic/update.php?YU5pWmc4YVNLVXVTTGJiTkdJRXNZeTJYOjEwOTQ2NTU0 >> /tmp/freedns_mx_kray_ca.log 2>&1 &
-fi
