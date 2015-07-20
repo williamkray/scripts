8cfb26209587a139ff5331bdbbfde6f78fde84af
William Kray <william.kray@gmail.com>
2015-07-20 14:49:19 -0700

    modified to use the actual ssh command running, so it will work for multiple hosts and ssh sessions

--- ssh-dup.sh
+++ ssh-dup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+## finds ssh sessions running in the current
+## tmux pane, and creates a duplicate
+## in a new pane. if the ssh session
+## makes use of the s script, it skips trying
+## to run that command and runs the s script anew.
 
-host=$(tmux display-message -p '#W')
+pane_pid=$(tmux display-message -p "#{pane_pid}")
+child_pids=$(pgrep -P $pane_pid)
+cmds=$(ps -o cmd -p "$child_pids")
+ssh_cmd=$(echo "$cmds"|egrep "ssh\s|s\s")
 
-/home/william/Scripts/s $host
+if [[ $(echo $ssh_cmd|grep "rightscale") ]]; then
+  host=$(tmux display-message -p '#W')
+  tmux split-window "/home/william/Scripts/s rightscale@$host"
+else
+  tmux split-window "$ssh_cmd"
+fi
