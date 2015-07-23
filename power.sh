cf41465bfcfcceaa9abcb2ae171670d0a918a28b
William Kray <william.kray@gmail.com>
2015-07-23 12:27:39 -0700

    added comment

--- power.sh
+++ power.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+## this is basically just a wrapper for
+## powerdown and powerup scripts (from the AUR)
+## to automatically figure out which one to run
+## and then run other commands afterwards.
 
 if [[ $(cat /sys/class/power_supply/AC/online) == 1 ]]; then
   sudo powerup
