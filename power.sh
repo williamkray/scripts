bbf316740af942913a289f5fdaf7b1f9ee06a981
William Kray <william.kray@gmail.com>
2015-08-14 17:54:13 -0700

    full path to powerscripts just in case

--- power.sh
+++ power.sh
@@ -5,9 +5,9 @@
 ## and then run other commands afterwards.
 
 if [[ $(cat /sys/class/power_supply/AC/online) == 1 ]]; then
-  sudo powerup
+  sudo /usr/bin/powerup
 else
-  sudo powerdown
+  sudo /usr/bin/powerdown
 fi
 
 sudo -u william /home/william/Scripts/autorandr.sh
