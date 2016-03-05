9210ebb4b3230f9a4e20373ff49445b5e0b29c66
William Kray <william.kray@gmail.com>
2016-03-05 14:17:23 -0800

    fix path

--- musicmount.sh
+++ musicmount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 fusermount -uz ~/Music
-sshfs kray:/media/storage/media/music ~/Music -o reconnect,ServerAliveInterval=5,ServerAliveCountMax=1
+sshfs kray:/mnt/media/music ~/Music -o reconnect,ServerAliveInterval=5,ServerAliveCountMax=1
