92442c6376892531962b0a7c0d5703412eb30a4b
William Kray <william.kray@gmail.com>
2016-03-13 09:16:51 -0700

    comments. a lot of them.

--- launch-vim.sh
+++ launch-vim.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+## this is just a wrapper, so that
+## desktop calls to launch gvim or something like that
+## will use my terminal emulator, create a session in tmux,
+## and launch vim to edit whatever file was selected.
+## I used to use it for a keyboard shortcut, but i 
+## think auto-tmux.sh has grown to handle this concept.
+## i still keep this around just for kicks though.
 
 /usr/bin/urxvt -e /home/william/Scripts/auto-tmux.sh "vim $1"
