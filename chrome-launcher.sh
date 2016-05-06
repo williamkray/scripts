221581827227ec6bb465f64943948b28b159d45c
William Kray <william.kray@gmail.com>
2016-05-05 21:09:38 -0700

    simpler naming scheme for desktops

--- chrome-launcher.sh
+++ chrome-launcher.sh
@@ -2,9 +2,9 @@
 
 workspace=$(wmctrl -d | grep \* | awk '{print $NF}')
 
-if [[ $workspace == "CIS" ]]; then
+if [[ $workspace == "W" ]]; then
   google-chrome-stable --profile-directory="Profile 5"
-elif [[ $workspace == "Opsflo" ]]; then
+elif [[ $workspace == "S" ]]; then
   google-chrome-stable --profile-directory="Profile 2"
 else
   google-chrome-stable --profile-directory="Profile 3"
