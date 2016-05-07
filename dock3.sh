e17f68d996e766625c58c1d924f01776f55c1fc8
William Kray <william.kray@gmail.com>
2016-05-07 11:19:08 -0700

    move position of primary monitor to match top of side mon

--- dock3.sh
+++ dock3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xrandr --output DP2-1 --primary  --mode 1920x1080  --pos 0x430 --rotate normal  
-xrandr --output eDP1 --mode 1920x1080 --pos 0x1510 --rotate normal  
+xrandr --output DP2-1 --primary  --mode 1920x1080  --pos 0x0 --rotate normal  
+xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal  
 sleep 1
 xrandr --output DP2-2 --mode 1920x1080  --pos 1920x0 --rotate left  
 fehbg.sh
