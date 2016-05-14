b04983c7a8eb3629622f524520bae418cbc45a21
William Kray <william.kray@gmail.com>
2016-05-14 14:00:57 -0700

    include dhcpcd for dock0 interface now that i have a udev rule for renaming it

--- auto-randr.sh
+++ auto-randr.sh
@@ -4,8 +4,10 @@ xrandr_output="$(xrandr)"
 
 if [ -n "$( echo \"$xrandr_output\"|grep 'DP2-1 connected')" ]; then
   dock3.sh
+  sudo dhcpcd dock0
 else
   undock.sh
+  sudo dhcpcd -k dock0
 fi
 
 auto-pulse.sh
