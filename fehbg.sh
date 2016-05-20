a932a58cc7719ed61d83ef1fc260d37ad079b8f6
William Kray <william.kray@gmail.com>
2016-05-20 15:56:35 -0700

    ditch geometric directory

--- fehbg.sh
+++ fehbg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY=:0.0 /usr/bin/feh --randomize --bg-fill --no-xinerama /home/william/.wallpapers/geometric/*
+DISPLAY=:0.0 /usr/bin/feh --randomize --bg-fill --no-xinerama /home/william/.wallpapers/*
