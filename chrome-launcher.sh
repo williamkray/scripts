70d95addd077bb8baf55924302096f7fe05d48b1
William Kray <william.kray@gmail.com>
2016-05-21 13:13:10 -0700

    include argument handling for chrome launcher

--- chrome-launcher.sh
+++ chrome-launcher.sh
@@ -3,9 +3,9 @@
 workspace=$(wmctrl -d | grep \* | awk '{print $NF}')
 
 if [[ $workspace == "W" ]]; then
-  google-chrome-stable --profile-directory="Profile 5"
+  google-chrome-stable --profile-directory="Profile 5" $@
 elif [[ $workspace == "S" ]]; then
-  google-chrome-stable --profile-directory="Profile 2"
+  google-chrome-stable --profile-directory="Profile 2" $@
 else
-  google-chrome-stable --profile-directory="Profile 3"
+  google-chrome-stable --profile-directory="Profile 3" $@
 fi
