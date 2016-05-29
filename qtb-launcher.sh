8eb697fb866933a782e34ac802a5a1d4cf5d0e1b
William Kray <william@tater.kray.ca>
2016-05-29 13:42:43 -0700

    desktop numbering, more features

--- qtb-launcher.sh
+++ qtb-launcher.sh
@@ -2,8 +2,10 @@
 
 workspace=$(wmctrl -d | grep \* | awk '{print $NF}')
 
-if [[ $workspace == "W" ]]; then
+if [[ $workspace == "2" ]]; then
   qutebrowser --basedir ~/.qutebrowser/corpinfo $@
+elif [[ $workspace == "4" ]]; then
+  qutebrowser --basedir ~/.qutebrowser/beachbody $@
 else
   qutebrowser $@
 fi
