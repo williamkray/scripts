61354e2a0daa42db7bc2a24ddae0fbed0069d5d9
William Kray <william.kray@gmail.com>
2016-06-24 14:07:17 -0700

    single monitor script and modified randr script, and some more colorful things to do in a terminal

--- auto-randr.sh
+++ auto-randr.sh
@@ -6,8 +6,12 @@ if [ -n "$( echo \"$xrandr_output\"|grep 'DP2-1 connected')" ]; then
   dock3.sh
   sudo dhcpcd dock0
 else
-  undock.sh
-  sudo dhcpcd -k dock0
+  if [ -n "$( echo \"$xrandr_output\"|grep ' DP1 connected')" ]; then
+    dock_bb.sh
+  else
+    undock.sh
+    sudo dhcpcd -k dock0
+  fi
 fi
 
 auto-pulse.sh
