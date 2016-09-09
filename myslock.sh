1cf6dd75231e03af4244644c5d9c76e37b4c309d
William Kray <william.kray@gmail.com>
2016-09-09 07:52:23 -0700

    temp irvine office dock add pulse, and change screen lock to i3lock

--- myslock.sh
+++ myslock.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-sleep .1 && slock -c "#8C9440" -d
+#sleep .1 && slock -c "#8C9440" -d
+file="/tmp/lock.png"
+
+revert() {
+  xset dpms 0 0 0
+}
+
+trap revert SIGHUP SIGINT SIGTERM
+
+#xset +dpms dpms 5 5 5
+scrot $file
+convert $file -scale 10% -scale 1000% $file
+i3lock -n -u -i $file
+
+#revert
