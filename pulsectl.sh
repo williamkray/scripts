727ba7c9253aa1956e66395074d2ae43c669f853
William Kray <william.kray@gmail.com>
2016-12-10 23:48:12 -0800

    make pulsectl a little more robust

--- pulsectl.sh
+++ pulsectl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+touch /tmp/vol
+nid=$(cat /tmp/vol)
 
 case "$1" in
   up)
@@ -10,3 +12,25 @@ case "$1" in
   toggle)
     amixer -D pulse set Master toggle
 esac
+
+master=$(amixer get Master|grep 'Front Left:')
+vol=$(echo "$master" | awk '{print $5}'|tr -d "[|]|%")
+state=$(amixer get Master|grep 'Front Left:'|awk '{print $6}'|tr -d "[|]|%")
+
+if [[ $state == "on" ]]; then
+  if (( "$vol" <= 30)); then
+    icon="notification-audio-volume-low"
+  elif (( "$vol" <= 60 && "$vol" > 30)); then
+    icon="notification-audio-volume-medium"
+  elif (( "$vol" <= 100 && "$vol" > 60)); then
+    icon="notification-audio-volume-high"
+  else
+    echo "volume can't be found!"
+  fi
+  vol=" $vol%"
+else
+  icon="notification-audio-volume-muted"
+  vol="MUTE"
+fi
+
+notify-desktop -r $nid -i $icon $vol > /tmp/vol
