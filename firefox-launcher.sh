41e4c715db5698242dfb66b9e89b783d8737ef95
William Kray <william@kray.pw>
2017-03-24 11:40:59 -0700

    change profile

--- firefox-launcher.sh
+++ firefox-launcher.sh
@@ -5,7 +5,7 @@ workspace=$(wmctrl -d | grep \* | awk '{print $NF}')
 if [[ $workspace == "2" ]]; then
   firefox -P corpinfo $@
 elif [[ $workspace == "4" ]]; then
-  firefox -P beachbody $@
+  firefox -P halliburton $@
 else
   firefox -P default $@
 fi
