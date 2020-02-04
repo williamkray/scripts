2bd2ad4d6f307e8b868e8d5bf8d94eb5d50c061e
William Kray <github@williamkray.com>
2020-02-03 16:06:48 -0800

    add basic initialization script

---
+++ init.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+## This script aims to download and configure basic settings
+## on a fresh linux system. I'm intentionally avoiding complex
+## bits like setting up openbox as this depends on lots of 
+## extra packages, focusing on primarily core level functionality.
+
+set -ex
+
+if ! [[ $(which git) ]]; then
+  echo "git is not installed. please install it and try running this script again."
+  exit 1
+fi
+
+## first thing's first, we need to download some git repos
+mkdir -p ~/git
+
+pushd ~/git
+  for repo in scripts dotfiles; do
+    if [[ -d ${repo} ]]; then
+      pushd ./${repo}
+        git pull
+      popd
+    else
+      git clone https://github.com/williamkray/${repo}.git
+    fi
+  done
+popd
+
+## create some symlinks to things if they don't already exist
+if ! [[ -L ~/.scripts ]]; then
+  ln -s ~/git/scripts ~/.scripts ## this one is important so things work right
+fi
+if ! [[ -L ~/.bashrc ]]; then
+  mv ~/.bashrc ~/.bashrc.orig && ln -s ~/git/dotfiles/bashrc ~/.bashrc
+fi
+if ! [[ -L ~/.bash_aliases ]]; then
+  ln -s ~/git/dotfiles/bash_aliases ~/.bash_aliases
+fi
+
+## do the needful for vim configuration
+mkdir -p ~/.vim/{colors,bundle,autoload}
+
+## download pathogen, my vimrc, and the hybrid colorscheme
+wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+wget -O ~/.vim/colors/hybrid.vim https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
+if ! [[ -L ~/.vim/vimrc ]]; then
+  ln -s ~/git/dotfiles/vimrc ~/.vim/vimrc
+fi
+
+pushd ~/.vim/bundle/
+  for pkg in \
+    https://github.com/Yggdroot/indentLine.git \
+    https://github.com/vim-scripts/LargeFile.git \
+    https://github.com/vim-airline/vim-airline.git \
+    https://github.com/vim-airline/vim-airline-themes.git \
+    https://github.com/tpope/vim-fugitive.git \
+    https://github.com/elzr/vim-json.git \
+    https://github.com/mhinz/vim-startify.git \
+    https://github.com/triglav/vim-visual-increment.git ; do
+      pkgdir=$(echo ${pkg} | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')
+      if [[ -d ./${pkgdir} ]]; then
+        pushd ./${pkgdir}
+          git pull
+        popd
+      else
+        git clone "$pkg"
+      fi
+  done
+popd
+
+## tmux!
+if ! [[ -L ~/.tmux.conf ]]; then
+  ln -s ~/git/dotfiles/tmux.conf ~/.tmux.conf
+fi
+
+## rofi!
+mkdir -p ~/.config
+if ! [[ -L ~/.config/rofi ]]; then
+  ln -s ~/git/dotfiles/rofi ~/.config/rofi
+fi
