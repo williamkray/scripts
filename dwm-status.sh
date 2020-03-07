d215733580ebe4fe668c10e390b1ac17e038eecc
William Kray <william@kray.pw>
2020-03-06 22:06:23 -0800

    fix dwm-status because i'm dumb

--- dwm-status.sh
+++ dwm-status.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-time_local=$(date +%I:%M%p)
-time_utc=$(date -u +%H:%M)
-
 while true; do
+
+  time_local=$(date +%I:%M%p)
+  time_utc=$(date -u +%H:%M)
+
   xsetroot -name "${time_local} [${time_utc}]"
   sleep 2
 done
