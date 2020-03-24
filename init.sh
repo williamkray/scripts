2f508dd8d20022d36ede6887a8882e79c88c53e9
William Kray <william@kray.pw>
2020-03-24 16:15:18 -0700

    symlink bash_profile

--- init.sh
+++ init.sh
@@ -69,6 +69,7 @@ popd
 ## create some symlinks to things if they don't already exist
 lnss ~/git/scripts ~/.scripts ## this one is important so things work right
 lnss ~/git/dotfiles/bashrc ~/.bashrc
+lnss ~/git/dotfiles/bash_profile ~/.bash_profile
 lnss ~/git/dotfiles/bash_aliases ~/.bash_aliases
 
 ## do the needful for vim configuration
