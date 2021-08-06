f867ecb25568f5d9da0cae7d5ab0d257d0c27a1f
William Kray <github@williamkray.com>
2021-08-06 13:22:20 -0700

    more commands, copying things to clipboard

--- myfeh.sh
+++ myfeh.sh
@@ -2,7 +2,9 @@
 
 feh --scale-down \
   --draw-tinted \
-  --action ';[Copy file name to clipboard]echo -n %N | xclip -in -selection clipboard && dunstify "feh" "image name copied to clipboard"' \
+  --action1 ';[Copy file name to clipboard]echo -n %N | xclip -in -selection clipboard && dunstify "feh" "image name copied to clipboard"' \
+  --action2 ';[Copy file path to clipboard]echo -n %F | xclip -in -selection clipboard && dunstify "feh" "image file path copied to clipboard"' \
+  --action ';[Copy file content to clipboard]xclip -in -selection clipboard -t image/png %F && dunstify "feh" "image copied to clipboard"' \
   --draw-actions  \
   --conversion-timeout 5 \
   --start-at $@
