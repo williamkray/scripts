5d4ee78a6dc4da89a3ceea92afa63dbbf3f80a17
William Kray <github@williamkray.com>
2021-08-06 15:02:40 -0700

    update screenlocker to use indicator again

--- screen-locker.sh
+++ screen-locker.sh
@@ -14,12 +14,12 @@ elif [[ $(which xautolock) && $(which i3lock) && $(which screencap.sh) ]]; then
     xset dpms 0 0 0
   }
   lock() {
-    i3lock -n -u -i $1 \
-    # --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
-    # --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
-    # --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-    # --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
-    # --radius=15 --veriftext="" --wrongtext=""
+    i3lock -n -i $1 \
+      --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+      --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+      --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
+      --ringver-color=ffffffff --ringwrong-color=ffffffff \
+      --radius=15 --verif-text="" --wrong-text=""
 
   }
 
