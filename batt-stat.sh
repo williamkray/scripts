f7755e3278095b3042614bb1efebfb245c20338a
William Kray <william@kray.pw>
2022-12-12 12:43:56 -0800

    make batt-stat more intelligent about whether there are multiple batteries

--- batt-stat.sh
+++ batt-stat.sh
@@ -1,6 +1,15 @@
  #!/usr/bin/env bash
 
- while true; do
-   paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | awk '{split($0,a,"="); split(a[2],b," "); (a[3] == "Charging" || b[1] == "Charging") ? $5 = "Charging" : $5 = (a[3] + b[1])/2; print a[1] "=" $5}' > ~/.uevent
-   sleep 5
- done
+if [[ $(ls -l /sys/class/power_supply/ | grep BAT | wc -l) -gt 1 ]]; then 
+  while true; do
+    paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | awk '{split($0,a,"="); split(a[2],b," "); (a[3] == "Charging" || b[1] == "Charging") ? $5 = "Charging" : $5 = (a[3] + b[1])/2; print a[1] "=" $5}' > ~/.uevent
+    sleep 5
+  done
+else
+  if [ -L ~/.uevent ]; then
+    if [ ! -e ~/.uevent ]; then
+      ln -s /sys/class/power_supply/BAT0/uevent ~/.uevent
+    fi
+  fi
+fi
+
