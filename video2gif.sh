972477d61b5e777c6fa7a2ca3da943cf55129594
William Kray <github@williamkray.com>
2023-06-23 09:21:55 -0700

    update vid2gif converter so scale isnt so tiny

--- video2gif.sh
+++ video2gif.sh
@@ -5,8 +5,8 @@
 # Convert video to gif file.
 # Usage: video2gif video_file (scale) (fps)
 video2gif() {
-  ffmpeg -y -i "${1}" -vf fps=${3:-20},scale=${2:-320}:-1:flags=lanczos,palettegen "${1}.png"
-  ffmpeg -i "${1}" -i "${1}.png" -filter_complex "fps=${3:-20},scale=${2:-320}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${1}".gif
+  ffmpeg -y -i "${1}" -vf fps=${3:-20},scale=${2:-640}:-1:flags=lanczos,palettegen "${1}.png"
+  ffmpeg -i "${1}" -i "${1}.png" -filter_complex "fps=${3:-20},scale=${2:-640}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${1}".gif
   rm "${1}.png"
 }
 
