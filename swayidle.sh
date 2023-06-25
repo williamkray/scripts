c9b83d1dd4be311fef2a2466af0d33bda079e369
William Kray <github@williamkray.com>
2023-06-25 07:37:54 -0700

    swayidle all the time

--- swayidle.sh
+++ swayidle.sh
@@ -4,4 +4,4 @@ swayidle \
   timeout 300 'screen-locker.sh' \
   timeout 600 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' \
-  before-sleep 'swaylock -c 000000' &
+  before-sleep 'screen-locker.sh' &
