2681dd62c03a6a4125b4f996e11fa78fd9cf2fc3
William Kray <github@williamkray.com>
2023-08-03 09:45:11 -0700

    try to get hyprland to not crash

--- hypr-laptop-lid.sh
+++ hypr-laptop-lid.sh
@@ -2,7 +2,13 @@
 set -x
 state_file=$(find /proc/acpi/button/lid/LID*/state)
 if grep -q open ${state_file}; then
-    hyprctl keyword monitor "eDP-1,preferred,auto,auto"
+    hyprctl keyword monitor "eDP-1,preferred,auto,1"
 else
-    hyprctl keyword monitor "eDP-1,disable"
+    mon_count=$(hyprctl -j monitors | jq '. | length')
+    echo $mon_count > /tmp/mon_count
+    if [[ $mon_count -gt 1 ]]; then
+        hyprctl keyword monitor "eDP-1,disable"
+    else
+        return 0
+    fi
 fi
